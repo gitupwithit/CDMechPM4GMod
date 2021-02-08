4e7742c1c47dc1310b7b9c16769e3b4e9954c125
Sun Zoo <32688796+gitupwithit@users.noreply.github.com>
2021-02-08 15:24:26 -0500

    1v
    See this tutorial for how I made the model this far:
    
    https://steamcommunity.com/sharedfiles/filedetails/?id=2043010348
    
    To fixed: 1st person arms, proportions, bodygroups for different coloured mechs

---
+++ for_workshop/sm/lua/autorun/sm.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Clone Drone Mech", "models/sm/sm.mdl" )
+player_manager.AddValidHands( "Clone Drone Mech", "models/sm/c_arms_citizen.mdl", 0, "00000000" )
+list.Set( "PlayerOptionsModel", "Clone Drone Mech", "models/sm/sm.mdl" )
